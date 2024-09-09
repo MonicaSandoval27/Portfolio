94ed8dbf8f300d420812bc0f8f409c412cbe1622
Monica <devmonicasandoval@gmail.com>
2024-09-09 14:57:09 -0700

    Grid Implementation

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
 import { Hero } from "@/components/Hero";
+import Grid from "@/components/Grid";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -8,7 +11,11 @@ export default function Home() {
     px-5"
     >
       <div className="max-w-7xl w-full">
+        <FloatingNav
+          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
+        />
         <Hero />
+        <Grid />
       </div>
     </main>
   );

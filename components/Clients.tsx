6bbe87b20c0e3a47d8437864998e67f98211c790
Monica <devmonicasandoval@gmail.com>
2024-09-12 12:25:42 -0700

    Testimonial progress

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,21 +1,31 @@
 import React from "react";
 import { InfiniteMovingCards } from "./ui/infiniteMovingCards";
-import { testimonials } from "@/data";
+import { companies, testimonials } from "@/data";
 
 const Clients = () => {
-  <div className="py-20" id="projects">
-    <h1 className="heading">
-      Kind words from{" "}
-      <span className="text-purple"> employers and coworkers</span>
-    </h1>
-    <div className="flex flex-col items-center">
-      <InfiniteMovingCards
-        items={testimonials}
-        direction="right"
-        speed="slow"
-      />
+  return (
+    <div className="py-20" id="testimonials">
+      <h1 className="heading">
+        Kind words from{" "}
+        <span className="text-purple"> employers and coworkers</span>
+      </h1>
+      <div className="flex flex-col items-center max-lg:mt-10">
+        <InfiniteMovingCards
+          items={testimonials}
+          direction="right"
+          speed="slow"
+        />
+        <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg: mt-10">
+          {companies.map(({ id, img, name, nameImg }) => (
+            <div key={id} className="flex md:max-w-60 max-w-32 gap-2">
+              <img src={img} alt={name} className="md:w-10 w-5" />
+              <img src={nameImg} alt={name} className="md:w-24 w-20" />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Clients;

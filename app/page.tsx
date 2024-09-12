cd47ae22ad6f742eb5ab1b8065e199639e4ec2a0
Monica <devmonicasandoval@gmail.com>
2024-09-12 11:23:02 -0700

    Progress with Testimonials

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { Hero } from "@/components/Hero";
 import Grid from "@/components/Grid";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
-import { FaHome } from "react-icons/fa";
 import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
 

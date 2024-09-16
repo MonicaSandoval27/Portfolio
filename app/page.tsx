63258f287dbfd039249d607fc5af2f1801b60188
Monica <devmonicasandoval@gmail.com>
2024-09-16 10:59:09 -0700

    Adding Approch

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
 
 export default function Home() {
   return (
@@ -20,6 +21,7 @@ export default function Home() {
         <RecentProjects />
         <Clients />
         <Experience />
+        <Approach />
       </div>
     </main>
   );
